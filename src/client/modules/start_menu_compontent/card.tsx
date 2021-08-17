ab6add9e6d0573e5acf0063385c146145c32794f
jhein_mac <superjamrs323@gmail.com>
2021-08-17 12:19:01 -0700

    Added some of the components for the start menu

---
+++ src/client/modules/start_menu_compontent/card.tsx
@@ -0,0 +1,127 @@
+import { SingleMotor } from "@rbxts/flipper";
+import Roact from "@rbxts/roact";
+
+interface CardComponentProps {
+    focusedMotor: SingleMotor;
+    position: Roact.Binding<UDim2>;
+}
+
+export class CardComponent extends Roact.Component<CardComponentProps> {
+    focused: Roact.Binding<number>;
+    constructor(props: CardComponentProps) {
+        super(props);
+        let setFocused: (x: number) => void;
+        [this.focused, setFocused] = Roact.createBinding(props.focusedMotor.getValue());
+        props.focusedMotor.onStep(setFocused);
+    }
+    render() {
+        return (
+            <frame
+                Key="Card"
+                BackgroundColor3={this.focused.map((x) =>
+                    Color3.fromRGB(55, 55, 74).Lerp(Color3.fromRGB(32, 32, 43), x),
+                )}
+                BorderColor3={Color3.fromRGB(125, 125, 168)}
+                Size={this.focused.map((x) => new UDim2(0.4, 0, 0.2, 0).Lerp(new UDim2(0.8, 0, 0.4, 0), x))}
+                SizeConstraint={Enum.SizeConstraint.RelativeYY}
+            >
+                <imagelabel
+                    Key="Image"
+                    BackgroundTransparency={1}
+                    Image="rbxassetid://7271977339"
+                    ImageTransparency={this.focused.map((x) => -x + 1)}
+                    Size={new UDim2(0.79, 0, 1, 0)}
+                    SizeConstraint={Enum.SizeConstraint.RelativeYY}
+                />
+                <textlabel
+                    Key="Title"
+                    BackgroundTransparency={1}
+                    Font={Enum.Font.SourceSansBold}
+                    Position={new UDim2(0.55, 0, 0.2, 0)}
+                    Size={new UDim2(0.3, 0, 0.1, 0)}
+                    TextColor3={this.focused.map((x) =>
+                        Color3.fromRGB(150, 150, 150).Lerp(Color3.fromRGB(255, 255, 255), x),
+                    )}
+                    TextScaled={true}
+                    TextSize={14}
+                    TextWrapped={true}
+                />
+                <textlabel
+                    Key="Description"
+                    BackgroundTransparency={1}
+                    Font={Enum.Font.SourceSans}
+                    Position={new UDim2(0.55, 0, 0.3, 0)}
+                    Size={new UDim2(0.3, 0, 0.5, 0)}
+                    Text="This is some text that you won't ever see"
+                    TextColor3={this.focused.map((x) =>
+                        Color3.fromRGB(150, 150, 150).Lerp(Color3.fromRGB(255, 255, 255), x),
+                    )}
+                    TextSize={18}
+                    TextWrapped={true}
+                    TextXAlignment={Enum.TextXAlignment.Left}
+                    TextYAlignment={Enum.TextYAlignment.Top}
+                />
+                <textbutton
+                    Key="GroupButton"
+                    BackgroundColor3={Color3.fromRGB(255, 255, 255)}
+                    Font={Enum.Font.SourceSansSemibold}
+                    Position={new UDim2(0.55, 0, 0.8, 0)}
+                    Size={new UDim2(0.15, -5, 0.06, 0)}
+                    Text="Group"
+                    TextColor3={Color3.fromRGB(32, 32, 43)}
+                    TextScaled={true}
+                    TextSize={12}
+                    TextWrapped={true}
+                >
+                    <uicorner CornerRadius={new UDim(0, 3)} />
+                    <uigradient
+                        Color={this.focused.map(
+                            (x) =>
+                                new ColorSequence([
+                                    new ColorSequenceKeypoint(
+                                        0,
+                                        Color3.fromRGB(26, 102, 135).Lerp(Color3.fromRGB(41, 171, 226), x),
+                                    ),
+                                    new ColorSequenceKeypoint(
+                                        1,
+                                        Color3.fromRGB(23, 112, 71).Lerp(Color3.fromRGB(34, 181, 115), x),
+                                    ),
+                                ]),
+                        )}
+                    />
+                </textbutton>
+                <textbutton
+                    Key="TeamButton"
+                    BackgroundColor3={Color3.fromRGB(255, 255, 255)}
+                    Font={Enum.Font.SourceSansSemibold}
+                    Position={new UDim2(0.7, 5, 0.8, 0)}
+                    Size={new UDim2(0.15, -5, 0.06, 0)}
+                    Text="Play"
+                    TextColor3={Color3.fromRGB(32, 32, 43)}
+                    TextScaled={true}
+                    TextSize={12}
+                    TextWrapped={true}
+                >
+                    <uicorner CornerRadius={new UDim(0, 3)} />
+                    <uigradient
+                        Color={this.focused.map(
+                            (x) =>
+                                new ColorSequence([
+                                    new ColorSequenceKeypoint(
+                                        0,
+                                        Color3.fromRGB(26, 102, 135).Lerp(Color3.fromRGB(41, 171, 226), x),
+                                    ),
+                                    new ColorSequenceKeypoint(
+                                        1,
+                                        Color3.fromRGB(23, 112, 71).Lerp(Color3.fromRGB(34, 181, 115), x),
+                                    ),
+                                ]),
+                        )}
+                    />
+                </textbutton>
+                <uicorner />
+                <uistroke Color={Color3.fromRGB(125, 125, 168)} />
+            </frame>
+        );
+    }
+}
---
+++ src/client/modules/start_menu_compontent/index.tsx
@@ -0,0 +1,46 @@
+import Roact from "@rbxts/roact";
+import { Workspace } from "@rbxts/services";
+
+interface StartMenuProps {}
+
+export class StartMenuComponent extends Roact.Component<StartMenuProps> {
+    leftCardPos: Roact.Binding<UDim2>;
+    rightCardPos: Roact.Binding<UDim2>;
+    centerCardPos: Roact.Binding<UDim2>;
+    setLeftCardPos: (position: UDim2) => void;
+    setRightCardPos: (position: UDim2) => void;
+    setCenterCardPos: (position: UDim2) => void;
+    leftRef = Roact.createRef<Frame>();
+    centerRef = Roact.createRef<Frame>();
+    constructor(props: StartMenuProps) {
+        super(props);
+        [this.leftCardPos, this.setLeftCardPos] = Roact.createBinding(new UDim2());
+        [this.rightCardPos, this.setRightCardPos] = Roact.createBinding(new UDim2());
+        [this.centerCardPos, this.setCenterCardPos] = Roact.createBinding(new UDim2());
+    }
+    updateCardPos(sideSize: Vector2, centerSize: Vector2) {
+        const center = this.getViewportCenter();
+        if (center) {
+            this.setLeftCardPos(UDim2.fromOffset(-sideSize.X / 2, center.Y - sideSize.Y / 2));
+            this.setRightCardPos(UDim2.fromOffset(center.X - sideSize.X / 2, center.Y - sideSize.Y / 2));
+            this.setLeftCardPos(UDim2.fromOffset(center.X - center.X / 2, center.Y - centerSize.Y / 2));
+        }
+    }
+    getViewportCenter() {
+        const camera = Workspace.CurrentCamera;
+        if (camera) return camera.ViewportSize.div(new Vector2(2, 2));
+    }
+    didMount() {
+        const leftSize = this.leftRef.getValue()?.AbsoluteSize;
+        const centerSize = this.centerRef.getValue()?.AbsoluteSize;
+        if (leftSize && centerSize)
+            Workspace.CurrentCamera?.GetPropertyChangedSignal("ViewportSize").Connect(() =>
+                this.updateCardPos(leftSize, centerSize),
+            );
+    }
+    render() {
+        return (
+            <screengui Key="Start Menu" ResetOnSpawn={false} ZIndexBehavior={Enum.ZIndexBehavior.Sibling}></screengui>
+        );
+    }
+}
